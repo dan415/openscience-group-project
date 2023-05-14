fc727d47411e6b2efdfe90f99bdfad86cf7b5ae7
Alejandro9998 <51530832+Alejandro9998@users.noreply.github.com>
2023-05-14 13:08:33 +0200

    Update ontology_classes.py

--- src/ontology_classes.py
+++ src/ontology_classes.py
@@ -1,38 +1,50 @@
 import xml.etree.ElementTree as ET
-
+import requests
+from wikidataintegrator import wdi_core, wdi_login
+from wikidataintegrator.wdi_helpers import try_write
 
 
 class Paper:
 
-    def __init__(self, tree=None, filename=None, pdf_path=None, xml_path=None, mode="xml", authors=None, title=None, journal=None):
+    def __init__(self, tree=None, filename=None, pdf_path=None, xml_path=None, physical=True, authors=None, title=None,
+                 journal=None, cited_by=None):
 
         self.input_path = pdf_path
         self.output_path = xml_path
         self.filename = filename
         self.tree = tree
-        if mode == "xml":
+        self.physical = physical
+        self.cluster = None
+        self.topic = None
+        self.acknowledgements = None
+        self.references = []
+        self.journal = None
+        self.cited_by = []
+        self.keywords = []
+        self.title = None
+        self.abstract = None
+        self.authors = []
+        self.journal = None
+        self.schema = None
+        if self.physical:
             self.schema = self.get_schema()
-            self.authors = self.get_authors() #  falla
+            self.authors = self.get_authors()
             self.abstract = self.get_abstract()
             self.acknowledgements = self.get_acknowledgements()
             self.references = self.get_references()
             self.keywords = self.get_keywords()
             self.title = self.get_title()
-        elif mode == "kg":  # knowledge graph
-            raise NotImplementedError("Knowledge graph mode not implemented yet")
-        elif mode == "citation":
-            self.authors = authors
-            self.title = title
-            self.journal = journal
+            self.title = self.title.lower() if self.title else self.title
         else:
-            raise ValueError("Mode must be either xml or kg")
-
+            self.authors = authors
+            self.title = title.lower() if title else title
+            self.journal = Journal(name=journal, publishes=[self]) if journal else None
+            self.cited_by = [cited_by]
     def get_schema(self):
         if not self.tree:
             return ""
         res = self.tree.getroot().tag.split("}")
         return res[0] + "}" if len(res) > 0 else ""
-
 
     def get_institution(self):
         if not self.tree:
@@ -64,8 +76,7 @@ class Paper:
         except:
             return ""
 
-
-    def get_author(self, author):
+    def get_author(self, author, add_as_writer=False):
         author_dict = {}
         if author.find(f"{self.schema}persName") is not None:
             if author.find(f"{self.schema}persName").find(f"{self.schema}forename") is not None:
@@ -85,7 +96,10 @@ class Paper:
                         f"{self.schema}country") is not None:
                     author_dict["affiliation_country"] = author.find(f"{self.schema}affiliation") \
                         .find(f'{self.schema}address').find(f"{self.schema}country").text
+        if add_as_writer:
+            author_dict["writes"] = [self]
         return Author(**author_dict)
+
     def get_authors(self):
         if not self.tree:
             return []
@@ -93,7 +107,7 @@ class Paper:
             authors = []
             for author in self.tree.findall(
                     f"{self.schema}teiHeader/{self.schema}fileDesc/{self.schema}sourceDesc//{self.schema}biblStruct/{self.schema}analytic/{self.schema}author"):
-                authors.append(self.get_author(author))
+                authors.append(self.get_author(author, add_as_writer=True))
             return authors
         except:
             return ""
@@ -123,23 +137,25 @@ class Paper:
 
     def get_acknowledgements(self):
         try:
-            return Aknowledgement(text=list(map(lambda x: [y.text for y in x], map(lambda x: [y for y in x.iter()], filter(
-                lambda elem: "acknowledgement" in list(elem.attrib.values()), [elem for elem in
-                                                                               self.tree.find(
-                                                                                   f"{self.schema}text").find(
-                                                                                   f"{self.schema}back").findall(
-                                                                                   rf"{self.schema}div")]))))[-1][-1])
+            return Aknowledgement(
+                text=list(map(lambda x: [y.text for y in x], map(lambda x: [y for y in x.iter()], filter(
+                    lambda elem: "acknowledgement" in list(elem.attrib.values()), [elem for elem in
+                                                                                   self.tree.find(
+                                                                                       f"{self.schema}text").find(
+                                                                                       f"{self.schema}back").findall(
+                                                                                       rf"{self.schema}div")]))))[-1][
+                    -1])
         except:
-            return Aknowledgement(text="")
+            return Aknowledgement(text="", source=self)
 
     def get_references(self):
         refs = []
-        refs_class = []
         if not self.tree:
             return refs
         for ref in self.tree.findall(
                 f"{self.schema}text/{self.schema}back/{self.schema}div/{self.schema}listBibl/{self.schema}biblStruct"):
             ref_dict = {}
+            ref_dict["source"] = self
             if ref.find(f"{self.schema}analytic") is not None:
                 if ref.find(f"{self.schema}analytic/{self.schema}title") is not None:
                     ref_dict["title"] = ref.find(f"{self.schema}analytic/{self.schema}title").text
@@ -147,50 +163,174 @@ class Paper:
                     aux_authors = [author for author in ref.findall(f"{self.schema}analytic/{self.schema}author")]
                     authors_class = []
                     for author in aux_authors:
-                        authors_class.append(self.get_author(author))
+                        authors_class.append(self.get_author(author, add_as_writer=False))
                     ref_dict["authors"] = authors_class
 
             if ref.find(f"{self.schema}monogr") is not None:
                 if ref.find(f"{self.schema}monogr/{self.schema}title") is not None:
-                    ref_dict["journal"] = ref.find(f"{self.schema}monogr/{self.schema}title").text
+                    if "title" in ref_dict.keys():
+                        ref_dict["journal"] = ref.find(f"{self.schema}monogr/{self.schema}title").text
+                    else:
+                        ref_dict["title"] = ref.find(f"{self.schema}monogr/{self.schema}title").text
                 if ref.find(f"{self.schema}monogr/{self.schema}imprint") is not None:
                     if ref.find(f"{self.schema}monogr/{self.schema}imprint/{self.schema}date") is not None:
                         ref_dict["date"] = ref.find(f"{self.schema}monogr/{self.schema}imprint/{self.schema}date").text
-            refs.append(ref_dict)
-            refs_class = [Citation(**ref_dict) for ref_dict in refs]
+
+                if "authors" not in ref_dict.keys():
+                    aux_authors = [author for author in ref.findall(f"{self.schema}monogr/{self.schema}author")]
+                    authors_class = []
+                    for author in aux_authors:
+                        authors_class.append(self.get_author(author, add_as_writer=False))
+                    ref_dict["authors"] = authors_class
+            if "title" in ref_dict.keys() and ref_dict["title"] is not None:
+                refs.append(ref_dict)
+        refs_class = [Citation(**ref_dict) for ref_dict in refs]
         return refs_class
 
 
 class Citation:
 
-    def __init__(self, date=None, authors=None, title=None, journal=None):
-        self.cites = Paper(authors=authors, title=title, journal=journal, mode="citation")
+    def __init__(self, date=None, authors=None, title=None, journal=None, source=None):
+        self.cites = Paper(authors=authors, title=title, journal=journal, physical=False, cited_by=self)
         self.date = date
+        self.source = source
+
+
 
 
 class Author:
+    
+    OPENALEX_API_URL = "https://openalex.org/api/v1/authors"
 
-    def __init__(self, forename=None, surname=None, email=None, affiliation_name=None, affiliation_country=None, twitter=None, orcid=None):
+    def __init__(self, forename=None, surname=None, email=None, affiliation_name=None, affiliation_country=None,
+                 twitter=None, orcid=None, writes=None, acknowledged_by=None):
+        if acknowledged_by is None:
+            acknowledged_by = []
+        if writes is None:
+            writes = []
         self.forename = forename
         self.surname = surname
         self.email = email
         self.twitter = twitter
         self.orcid = orcid
+        self.writes = writes
         self.affiliation = Affiliation(affiliation_name, affiliation_country)
+        self.ackowledged_by = acknowledged_by
+    
+    def get_openalex_info(self, author_id):
+        response = requests.get(f"{self.OPENALEX_API_URL}/{author_id}")
+        if response.status_code == 200:
+            author_data = response.json()
+            return {
+                "twitter": author_data["twitter"],
+                "orcid": author_data["orcid"],
+                "email": author_data["email"]
+            }
+        else:
+            return {}
+
+    def __eq__(self, other):
+        return self.forename == other.forename and self.surname == other.surname
+
+    def __hash__(self):
+        return hash((self.forename, self.surname))
 
 
 
 
 class Affiliation:
 
-    def __init__(self, name=None, country=None, established=None, website=None):
+    def __init__(self, name=None, country=None, established=None, website=None, acknowledged_by=[]):
         self.name = name
         self.country = country
         self.website = website
         self.established = established
+        self.acknowledged_by = acknowledged_by
+
+        if name:
+            wd_item_id = self.get_wikidata_item_id(name)
+            if wd_item_id:
+                affiliation_info = self.get_wikidata_info(wd_item_id)
+                self.website = affiliation_info.get("website")
+                self.established = affiliation_info.get("established")
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    @staticmethod
+    def get_wikidata_item_id(name):
+        query = f'SELECT ?item WHERE {{ ?item rdfs:label "{name}"@en }}'
+        results = wdi_core.WDItemEngine.execute_sparql_query(query)
+        if results["results"]["bindings"]:
+            return results["results"]["bindings"][0]["item"]["value"].split("/")[-1]
+        else:
+            return None
+
+    @staticmethod
+    def get_wikidata_info(wd_item_id):
+        query = f'SELECT ?website ?established WHERE {{ wd:{wd_item_id} wdt:P856 ?website . OPTIONAL {{ wd:{wd_item_id} wdt:P571 ?established }} }}'
+        results = wdi_core.WDItemEngine.execute_sparql_query(query)
+        website = results["results"]["bindings"][0]["website"]["value"] if results["results"]["bindings"][0]["website"] else None
+        established = results["results"]["bindings"][0]["established"]["value"] if results["results"]["bindings"][0].get("established") else None
+        return {"website": website, "established": established}
+
+
+
+
+from wikidataintegrator import wdi_core, wdi_login
+from wikidataintegrator.wdi_helpers import try_write
+
+class Journal:
+
+    def __init__(self, name=None, country=None, established=None, description=None, publishes=None):
+        if publishes is None:
+            publishes = []
+        self.name = name
+        self.country = country
+        self.description = description
+        self.established = established
+        self.publishes = publishes
+
+        if name:
+            wd_item_id = self.get_wikidata_item_id(name)
+            if wd_item_id:
+                journal_info = self.get_wikidata_info(wd_item_id)
+                self.country = journal_info.get("country_of_origin")
+                self.description = journal_info.get("description")
+                self.established = journal_info.get("established")
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    @staticmethod
+    def get_wikidata_item_id(name):
+        query = f'SELECT ?item WHERE {{ ?item rdfs:label "{name}"@en }}'
+        results = wdi_core.WDItemEngine.execute_sparql_query(query)
+        if results["results"]["bindings"]:
+            return results["results"]["bindings"][0]["item"]["value"].split("/")[-1]
+        else:
+            return None
+
+    @staticmethod
+    def get_wikidata_info(wd_item_id):
+        query = f'SELECT ?description ?established ?country_of_origin WHERE {{ wd:{wd_item_id} wdt:P31 wd:Q5633421 . OPTIONAL {{ wd:{wd_item_id} wdt:P17 ?country . ?country rdfs:label ?country_of_origin filter(lang(?country_of_origin) = "en") }} . OPTIONAL {{ wd:{wd_item_id} schema:description ?description filter(lang(?description) = "en") }} . OPTIONAL {{ wd:{wd_item_id} wdt:P571 ?established }} }}'
+        results = wdi_core.WDItemEngine.execute_sparql_query(query)
+        country_of_origin = results["results"]["bindings"][0]["country_of_origin"]["value"] if results["results"]["bindings"][0]["country_of_origin"] else None
+        description = results["results"]["bindings"][0]["description"]["value"] if results["results"]["bindings"][0].get("description") else None
+        established = results["results"]["bindings"][0]["established"]["value"] if results["results"]["bindings"][0].get("established") else None
+        return {"country_of_origin": country_of_origin, "description": description, "established": established}
 
 
 class Aknowledgement:
 
-    def __init__(self, text=None):
+    def __init__(self, text=None, source=None):
         self.text = text
+        self.source = source
+        self.acknowledges_org = []
+        self.acknowledges_people = []
