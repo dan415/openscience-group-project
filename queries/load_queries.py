a3b8c088f4b060af3b3f824daf2c6ee2d04aaec2
AlexAyuso01 <alejandro.ayusoex@icloud.com>
2023-05-15 11:56:52 +0200

    sparql queries

---
+++ queries/load_queries.py
@@ -0,0 +1,15 @@
+def load_queries(file_path):
+    with open(file_path, "r") as file:
+        queries = file.read()
+    return queries
+
+# Cargar consultas desde el archivo
+queries = load_queries("queries/sparql_queries.txt")
+
+# Ejecutar una consulta SPARQL utilizando la librería RDFlib
+results = g.query(queries)
+
+# Procesar los resultados de la consulta
+for row in results:
+    # Realizar operaciones con los resultados
+    print(row)
